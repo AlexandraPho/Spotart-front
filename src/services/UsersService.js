f27f6221bee278eb8d410efe15f73e31b7b5585c
ahlammamoun <mestar.ale@gmail.com>
2022-03-04 19:12:32 +0100

    few-changes

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -22,6 +22,16 @@ export default {
             .then(
                 (response) => {
                     console.log(response);
+                    store.commit('setToken', response.data.data.token);
+                    store.commit('setUserID', response.data.data.id);
+                    this.getRole();
+                    //store.commit('setUserRole', response.data.data.id);
+    
+                    // On execute la callback avec un objet contenant le succes
+                    callback({
+                        "type": "success",
+                        "message": response.data.message
+                    });
                 }
             );
     },
