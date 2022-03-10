59c09e46cefe483fc647da438a644b182a844cf1
alexandra2103 <pinheiro.alexandra21@gmail.com>
2022-03-10 18:49:47 +0100

    fix conflicts

--- src/router/index.js
+++ src/router/index.js
@@ -2,33 +2,19 @@ import {
   createRouter,
   createWebHistory
 } from 'vue-router'
-import SingleArtwork from '../views/SingleArtwork.vue'
+
 import Home from '../views/Home.vue'
 
-
-<<<<<<< HEAD
-
-
-
-
-
-=======
->>>>>>> bd90ff6c0f8cf78dfddc8f02d1117d64b50fa933
-const routes = [{
+  const routes = [{
     path: '/',
     name: 'Home',
     component: Home
-<<<<<<< HEAD
-=======
-
->>>>>>> bd90ff6c0f8cf78dfddc8f02d1117d64b50fa933
   },
   {
     path: '/artwork/:id',
     name: 'SingleArtwork',
-    component: SingleArtwork
+    component: () => import( /* webpackChunkName: "connection" */ '../views/SingleArtwork.vue')
   },
-<<<<<<< HEAD
   {
     path: '/publishartwork',
     name: 'PublishArtwork',
@@ -38,21 +24,16 @@ const routes = [{
     component: () => import( /* webpackChunkName: "connection" */ '../views/user/PublishArtwork.vue')
   },
   {
-    path: '/editartwork',
-    name: 'EditArtwork',
-=======
-{
     path: '/artwork/create',
     name: 'CreateArtwork',
     component: () => import( '../views/user/CreateArtwork.vue')
-},
-{
+  },
+  {
     path: '/artwork/update/'/*[i:artwokID]*/,
     name: 'UpdateArtwork',
     component: () => import( '../views/user/UpdateArtwork.vue')
-},
-
-{
+  },
+  {
     path: '/artist/:id',
     name: 'Artist',
     component: () => import( /*   webpackChunkName: "connection" */ '../views/ArtistProfile.vue')
@@ -68,17 +49,11 @@ const routes = [{
   {
     path: '/category/:id',
     name: 'Category',
->>>>>>> bd90ff6c0f8cf78dfddc8f02d1117d64b50fa933
-    // route level code-splitting
-    // this generates a separate chunk (category.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-<<<<<<< HEAD
-    component: () => import( /* webpackChunkName: "connection" */ '../views/user/EditArtwork.vue')
+    component: () => import(/* webpackChunkName: "category" */ '../views/Category.vue')
   },
   {
     path: '/artist/:id',
     name: 'Artist',
-
     component: () => import( /*   webpackChunkName: "connection" */ '../views/ArtistProfile.vue')
   },
   {
@@ -92,20 +67,13 @@ const routes = [{
   {
     path: '/connection',
     name: 'Connection',
-
     component: () => import(  '../views/ConnectionUser.vue')
   },
   {
     path: '/user/:id',
     name: 'UserAccount',
-
     component: () => import( /* webpackChunkName: "connection" */ '../views/UserProfile.vue')
   }
-=======
-    component: () => import(/* webpackChunkName: "category" */ '../views/Category.vue')
-},
->>>>>>> bd90ff6c0f8cf78dfddc8f02d1117d64b50fa933
-
 ]
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
