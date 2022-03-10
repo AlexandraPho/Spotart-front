ac8924235a9dc4b6e3fcc00db18134eae9ac2437
alexandra2103 <pinheiro.alexandra21@gmail.com>
2022-03-10 20:20:20 +0100

    SingleArtwork & UserProfil files

--- src/views/SingleArtwork.vue
+++ src/views/SingleArtwork.vue
@@ -1,25 +1,25 @@
 <template>
-  <main class="main-container">
   <HeaderLayout />
-  <InformationsLayout  v-if="artwork" v-bind:category="artwork._embedded['wp:term'][0][0].name" v-bind:author="artwork._embedded['author'][0].name" v-bind:image="artwork._embedded['wp:featuredmedia'][0].source_url" v-bind:title="artwork.title.rendered" v-bind:content="artwork.content.rendered" v-bind:date="artwork.date" v-bind:key="artwork.title" />
+  <InformationsLayout  v-if="artwork" v-bind:id="artwork.id" v-bind:category="artwork._embedded['wp:term'][0][0].name" v-bind:author="artwork._embedded['author'][0].name" v-bind:image="artwork._embedded['wp:featuredmedia'][0].source_url" v-bind:title="artwork.title.rendered" v-bind:content="artwork.content.rendered" v-bind:date="artwork.date" v-bind:key="artwork.title" />
+  <GalleryLayoutTitle  v-if="artwork" v-bind:categoryId="artwork._embedded['wp:term'][0][0].id" v-bind:category="artwork._embedded['wp:term'][0][0].name" />
   <FooterLayout />
-  </main>
 </template>
 
 <script>
 import HeaderLayout from '@/components/HeaderLayout.vue'
 import FooterLayout from '@/components/FooterLayout.vue'
-import InformationsLayout from '@/components/InformationsLayout.vue'
-
+//import ConnexionFormLayout from '@/components/UserProfile/ConnexionFormLayout.vue'
+import InformationsLayout from '@/components/SingleArtwork/InformationsLayout.vue'
+import GalleryLayoutTitle from '@/components/SingleArtwork/GalleryLayoutTitle.vue'
 // we import the service so we can have the datas from the WP API
 import ArtworksService from '@/services/ArtworksService.js';
-
 export default {
     name: 'SingleArtwork',
     components: {
     HeaderLayout,
     FooterLayout,
-    InformationsLayout
+    InformationsLayout,
+    GalleryLayoutTitle
     },
       data() {
       return {
@@ -27,10 +27,9 @@ export default {
           }
     },
     mounted() {
-    console.log(this.$route.params.id); 
+    //console.log(this.$route.params.id); 
     // vue router allow us to acces the context of the page => here we can access the id of the artwork in the URL
     //? carefull we are not dealing with all the errors - 404 ?
-
     ArtworksService.find(this.$route.params.id).then(
         // We are using the find functions with the id artwork in the url 
         // and recupering the artwork object to use it (display in other component)
@@ -39,10 +38,10 @@ export default {
             this.artwork = response.data;
         }
     );
-
     }
 }
 </script>
 
 <style src="@/assets/CSS/artwork.css">
 </style>
+
