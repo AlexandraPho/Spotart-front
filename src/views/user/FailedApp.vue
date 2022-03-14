5d03740ae62d14b4e1fada10296bbbc6625f5220
ahlammamoun <mestar.ale@gmail.com>
2022-03-14 13:09:32 +0100

    404wip

--- src/views/user/FailedApp.vue
+++ src/views/user/FailedApp.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-   
+    <header-layout />
     <failed-layout />
- 
+    <footer-layout />
   </div>
 </template>
 
 <script>
-
+import HeaderLayout from "@/components/template/HeaderLayout.vue";
 import FailedLayout from "@/components/user/FailedLayout.vue";
-
+import FooterLayout from "@/components/template/FooterLayout.vue";
 
 export default {
   name: "FailedApp",
   components: {
-  
+    HeaderLayout,
     FailedLayout,
-   
+    FooterLayout,
   },
 };
 </script>
+  
