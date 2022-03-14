9668426ebee8fed298556a9ad6df348c336cef6d
ahlammamoun <mestar.ale@gmail.com>
2022-03-14 11:22:30 +0100

    404

--- src/views/user/FailedApp.vue
+++ src/views/user/FailedApp.vue
@@ -1,42 +1,22 @@
 <template>
-  <section class="oops">
-    <p class="text">Je n'existe pas!</p>
-  </section>
-
-  <section class="picture">
-    <img
-      src="https://cdn.shopify.com/s/files/1/0255/3184/2642/articles/citation-licorne-827374_1193x.jpg?v=1574260431"
-    />
-  </section>
-  <section class="smoke">
-    <img
-      class="smoke"
-      src="https://us.123rf.com/450wm/lightfieldstudios/lightfieldstudios1901/lightfieldstudios190101113/114406991-m%C3%A9lange-d-%C3%A9claboussures-de-peinture-bleue-et-rose-isolated-on-white.jpg?ver=6"
-    />
-  </section>
-
-  <a href="./">Retour à l'Acceuil</a>
-  <section class="buble">
-    <img
-      class="buble"
-      src="https://us.123rf.com/450wm/jeremywhat/jeremywhat1202/jeremywhat120200442/12301076-pens%C3%A9e-ou-bulle-peut-%C3%AAtre-utilis%C3%A9-comme-un-espace-de-texte-ou-dans-une-bande-dessin%C3%A9e.jpg?ver=6"
-    />
-  </section>
-  <section class="text">
-    <p class="1">
-      Salut Vous , qu'est ce que vous faites ici, c'est un salon imaginaire le
-      404, réservé aux animaux légendaires...
-    </p>
-    <p class="2">
-      Vous pouvez retourner à l'acceuil et Shut! si Morgan vous demande , vous
-      ne m'avez pas vu
-    </p>
-  </section>
+  <div>
+   
+    <failed-layout />
+ 
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
 
-<style src="@/assets/css/failed.css">
-</style>
+import FailedLayout from "@/components/user/FailedLayout.vue";
+
+
+export default {
+  name: "FailedApp",
+  components: {
+  
+    FailedLayout,
+   
+  },
+};
+</script>
