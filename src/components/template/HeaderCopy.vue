0ef4c222bc1ff54bb86d6c9b84a021943601e180
alexandra2103 <pinheiro.alexandra21@gmail.com>
2022-04-08 13:15:28 +0200

    headerCopy totally responsive

--- src/components/template/HeaderCopy.vue
+++ src/components/template/HeaderCopy.vue
@@ -10,15 +10,13 @@
         </ul>
         <div class="right-header">
             <button class="icon-account" ref="menu" @click="isOpen = !isOpen">Mon compte</button>
-                <!--<transition name="fade" apear>-->
                     <div class="account-choice" v-if="isOpen">
                         <div class ="menu-item" ><router-link class="registration" v-if="!this.$store.state.token" to="/registration">Inscription</router-link></div>
                         <div class ="menu-item" ><router-link class="connexion-link" v-if="!this.$store.state.token" to="/login">Connexion</router-link></div>
-                        <div class ="menu-item" ><router-link class="create" v-if="this.$store.state.token && this.$store.getters.checkUserRole(['artist'])" to='/artworks/create'>Publiez vos oeuvres</router-link></div>
-                        <div class ="menu-item" ><router-link class="update" v-if="this.$store.state.token && this.$store.getters.checkUserRole(['artist'])" to='/artworks/update/:id'>Modifiez votre oeuvre</router-link></div>
+                        <div class ="menu-item" ><router-link class="create" v-if="this.$store.state.token && this.$store.getters.checkUserRole(['artist'])" to='/artworks/create'>Partagez vos créations</router-link></div>
+                        <div class ="menu-item" ><router-link class="update" v-if="this.$store.state.token && this.$store.getters.checkUserRole(['artist'])" to='/artworks/update/:id'>Modifiez vos publications</router-link></div>
                         <button class="connexion-link" @click="disconnect" v-if="this.$store.state.token">Deconnexion</button>
                     </div>
-                <!--</transition>-->
             <!-- <div>  <router-link class="connexion-link"  v-if="this.$store.state.token == null" to="/connection">{{ this.$store.state.token == null ? 'Connexion' : 'mon compte'  }}</router-link> -->
         </div>
     </header>
@@ -72,23 +70,20 @@
 
     @media (min-width: 320px) and (max-width: 768px) {
 
-        
-    }
-
-    @media (min-width: 768px) and (max-width: 1200px) {
-
+        /*Left side*/
         header
         {
             background-color: black;
             display: inline-flex;
-            justify-content: space-between;
+            justify-content: space-around;
+            align-items: center;
             width: 100%;
         }
 
         header h1
         {
-            margin: 10px 0 10px 20px;
-            font-size: x-large;
+            margin: 1.5rem;
+            font-size: xx-large;
             background: linear-gradient(to bottom right, white 60%,#ffde59  40%);
             background-size: auto auto;
             background-clip: border-box;
@@ -100,7 +95,121 @@
             color: white;
             cursor: pointer;
             display: inline-block;
-            flex-grow: 1;
+        }
+        @keyframes textclip {
+            to {
+                background-position: 200% center;
+            }
+        }
+
+        header h1 a
+        {
+            text-decoration: none;
+        }
+
+        /*Center*/
+        .header-links-list {
+            padding: 0;
+            text-align: center;
+            padding: 0;
+        }
+
+        .header-anchor-element {
+            text-decoration: none;
+            color: white;
+            font-size: 1rem;
+        }
+
+        .header-anchor-element:hover {
+            color: #ffde59;
+        }
+
+        /*Right side*/
+        .right-header 
+        {
+            margin: 2rem;
+            margin-left: 2rem;
+            position: relative;
+            display: inline-block; 
+        }
+
+        .icon-account {
+            color:#ffde59 ;
+            font-size: 0.9rem;
+            padding: 0.2rem;
+            border-radius: 0.5rem;
+            border: solid 0.1rem #ffde59;
+            text-decoration: none;
+            position: static;
+        }
+
+        .icon-account,
+        .account-choice {
+            background-color: black;
+        }
+
+        .account-choice {
+            position: absolute;
+            background-color: black;
+            box-shadow: 0px 8px 16px 0px rgb(0 0 0 / 20%);
+            padding: 0.5rem;
+        }
+
+        .menu-item a {
+            text-decoration: none;
+            color: #ffde59;
+        }
+
+        .menu-item,
+        .connexion-link,
+        .create {
+            margin-top: 0.5rem;
+        }
+
+        .connexion-link,
+        .create {
+            background-color: black;
+            color: #ffde59;
+            border: none;
+            font-size: 1rem;
+        }
+
+        .registration:hover,
+        .connexion-link:hover,
+        .create:hover,
+        .update:hover {
+            color: white;
+            cursor: pointer;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 1200px) {
+
+        /*Left side*/
+        header
+        {
+            background-color: black;
+            display: inline-flex;
+            justify-content: space-around;
+            align-items: center;
+            width: 100%;
+        }
+
+        header h1
+        {
+            margin: 1.5rem;
+            font-size: xx-large;
+            background: linear-gradient(to bottom right, white 60%,#ffde59 40%);
+            background-size: auto auto;
+            background-clip: border-box;
+            background-size: 200% auto;
+            background-clip: text;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            -webkit-animation: textclip-06fcd9d2 15s linear infinite;
+            animation: textclip-06fcd9d2 15s linear infinite;
+            color: white;
+            cursor: pointer;
         }
         @keyframes textclip {
             to {
@@ -123,24 +232,22 @@
             transform:scale(1.1);
         }
 
+        /*Center*/
         .header-links-list
         {
-            margin: 10px 0 10px 0;
+            margin: 0.5rem;
             display: flex;
-            flex-direction: row;
-            align-items: center;
             justify-content: center;
             list-style: none;
             gap: 15px;
             padding: 0;
-            flex-grow: 3;
         }
 
         .header-anchor-element
         {
             text-decoration: none;
             color: white;
-            font-size: large;
+            font-size: x-large;
         }
 
         .header-anchor-element:hover
@@ -148,50 +255,62 @@
             color: #ffde59;
         }
 
+        /*Right side*/
         .right-header
         {
-            display: flex;
-            align-items: center;
-            gap: 10px;
-            margin: 10px 20px 10px 0;
+            margin: 2rem;
+            margin-left: 2rem;
+            position: relative;
+            display: inline-block;
         }
 
-        .connexion-link
-        {
-            color: white;
-            font-size: 1rem;
+        .icon-account {
+            color:#ffde59 ;
+            font-size: 0.9rem;
+            padding: 0.2rem;
+            border-radius: 0.5rem;
+            border: solid 0.1rem #ffde59;
             text-decoration: none;
+            position: static;
         }
 
-        .signup
-        {
-        display: none;
-        }
-
-        .right-header button:hover
-        {
-            background-color: #ffde59;
-            color: black;
-            cursor: pointer;
-        }
-
-        .connexion-link:hover
-        {
-            color: #ffde59;
-        }
-
-        .publish-button
-        {
-            color: #ffde59;
-            border: solid 3px #ffde59;
-            border-radius: 10px;
+        .icon-account,
+        .account-choice {
             background-color: black;
         }
 
-        .publish-button:hover
-        {
-            background-color: #ffde59;
-            color: black;
+        .account-choice {
+            position: absolute;
+            background-color: black;
+            box-shadow: 0px 8px 16px 0px rgb(0 0 0 / 20%);
+            padding: 0.5rem;
+            z-index: 1;
+        }
+
+        .menu-item a {
+            text-decoration: none;
+            color: #ffde59;
+        }
+
+        .menu-item,
+        .connexion-link,
+        .create {
+            margin-top: 0.5rem;
+        }
+
+        .connexion-link,
+        .create {
+            background-color: black;
+            color: #ffde59;
+            border: none;
+            font-size: 1rem;
+        }
+
+        .registration:hover,
+        .connexion-link:hover,
+        .create:hover,
+        .update:hover {
+            color: white;
             cursor: pointer;
         }
     }
@@ -201,25 +320,26 @@
         header {
             background-color: black;
             display: inline-flex;
-            justify-content: space-between;
+            justify-content: space-around;
+            align-items: center;
             width: 100%;
         }
 
         header h1 {
-            margin: 10px 0 10px 30px;
+            margin: 2rem;
             font-size: xxx-large;
-            background: linear-gradient(to bottom right, white 60%,#ffde59  40%);
+            background: linear-gradient(to bottom right, white 60%,#ffde59 40%);
             background-size: auto auto;
             background-clip: border-box;
             background-size: 200% auto;
             background-clip: text;
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
-            animation: textclip 15s linear infinite;
+            -webkit-animation: textclip-06fcd9d2 15s linear infinite;
+            animation: textclip-06fcd9d2 15s linear infinite;
             color: white;
             cursor: pointer;
-            display: inline-block;
-            flex-grow: 1;
+            
         }
         @keyframes textclip {
             to {
@@ -241,15 +361,12 @@
         }
 
         .header-links-list {
-            margin: 10px 260px 10px 0;
             display: flex;
-            flex-direction: row;
-            align-items: center;
             justify-content: center;
             list-style: none;
             gap: 20px;
             padding: 0;
-            flex-grow: 3;
+            margin: 2rem;
         }
 
         .header-anchor-element {
@@ -264,7 +381,7 @@
 
         .right-header {
             /* align-items: center; */
-            margin: 1.5rem 3rem;
+            margin: 2rem;
             position: relative;
             display: inline-block;      
         }
@@ -291,6 +408,7 @@
             box-shadow: 0px 8px 16px 0px rgb(0 0 0 / 20%);
             margin-right: 3rem;
             padding: 0.5rem;
+            z-index: 1;
         }
 
         .menu-item a {
